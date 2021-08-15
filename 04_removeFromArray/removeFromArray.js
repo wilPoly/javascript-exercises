10d08f742b08d090dffea4df5b1841595bd3a73f
wilpoly <misterwrinkle@gmail.com>
2021-08-15 19:35:29 +0200

    do exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removed) {
+	for (let i = 0; i < removed.length; i++) {
+		for (let j = 0; j < array.length; j++) {
+			if (removed[i] === array[j]) {
+				array.splice(j, 1);
+			}
+		}
+	}
+	return array;
 };
 
 module.exports = removeFromArray;
