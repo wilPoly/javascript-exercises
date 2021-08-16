fed996fd9ffebfe999c3ed5cea73a216eb6208a4
wilpoly <misterwrinkle@gmail.com>
2021-08-16 12:29:25 +0200

    Do tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+	let tempCelsius = Number(((temp - 32) * 5 / 9).toFixed(1));
+	return (tempCelsius === 0.0) ?  0 : tempCelsius;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+	let tempFaranheit = Number((temp * 9 / 5 + 32).toFixed(1));
+	return (tempFaranheit === 0.0) ? 0 : tempFaranheit;
 };
 
 module.exports = {
