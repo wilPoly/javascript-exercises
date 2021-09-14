3b6eaf83c0e0a9a66fd9ce3bebcde33efd2d9644
wilpoly <misterwrinkle@gmail.com>
2021-09-14 21:05:07 +0200

    Do calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let total = 0;
+	return total = a.reduce((previous, current) => previous + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let total = 0;
+	return total = array.reduce((previous, current) => previous * current);
 };
 
-const power = function() {
-	
+const power = function(number, powerUp) {
+	return number ** powerUp;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a < 1) {
+		return 1;
+	}
+	else {
+		return a * factorial(a - 1);
+	}
 };
 
 // Do not edit below this line
