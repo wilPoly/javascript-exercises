904ed515c482604db64d561341c8019cc2ff65b5
wilpoly <misterwrinkle@gmail.com>
2021-09-15 19:40:34 +0200

    do fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	let noPunc = /\w/g;
+	string = string.toLowerCase().match(noPunc);
+	// console.log("original", string.join(""));
+	
+	let newString = string;
+	newString.reverse();
+	// console.log("mutated", newString.join(""));
+	for (let i = 0; i < string.length; i++) {
+		if (string[i] !== newString[i]) {
+			return false;
+		}
+		else continue;
+	}
+	return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	let result = 0;
+	n = Number(n);
+	if (n < 0) return "OOPS";
+	else if (n === 1) result = 1;
+	else if (n >= 2) {
+		result = fibonacci(n - 2) + fibonacci(n - 1);
+	}
+	return result;
 };
 
 // Do not edit below this line
