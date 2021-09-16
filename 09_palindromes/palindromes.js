d0da193414cb3b314ccd07b4a06fc3a265c6179f
wilpoly <misterwrinkle@gmail.com>
2021-09-16 22:19:32 +0200

    findTheOldest

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,7 @@
 const palindromes = function (string) {
-	let noPunc = /\w/g;
-	string = string.toLowerCase().match(noPunc);
-	// console.log("original", string.join(""));
-	
-	let newString = string;
-	newString.reverse();
-	// console.log("mutated", newString.join(""));
-	for (let i = 0; i < string.length; i++) {
-		if (string[i] !== newString[i]) {
-			return false;
-		}
-		else continue;
-	}
-	return true;
+	let originalString = string.toLowerCase().replace(/[^\w]/g, "");
+	let newString = originalString.split("").reverse().join("")
+	return (newString === originalString);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	const now = new Date().getFullYear();
 
+	const newPeople = people.map(person => {
+		if ("yearOfDeath" in person) {
+			return person;
+		}
+		else {
+			person.yearOfDeath = now;
+			return person;
+		}
+	});
+	console.table(newPeople);
+
+	const oldest = newPeople.sort((firstPerson, secondPerson) => {
+		return (firstPerson.yearOfDeath - firstPerson.yearOfBirth) > 
+		(secondPerson.yearOfDeath - secondPerson.yearOfBirth) ? -1 : 1;
+	});
+	console.table(oldest);
+	
+	return oldest[0];
 };
 
 // Do not edit below this line
